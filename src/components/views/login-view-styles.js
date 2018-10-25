2d42f02e805eaf0e8341d372a1a8d672b2525e8e
Taylor <15224340+ttstauss@users.noreply.github.com>
2018-10-25 11:45:20 -0600

    refactor LoginView styles and convert input to controlled

---
+++ src/components/views/login-view-styles.js
@@ -0,0 +1,91 @@
+/**
+@license
+Copyright (c) 2018 The Polymer Project Authors. All rights reserved.
+This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+Code distributed by Google as part of the polymer project is also
+subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+*/
+
+/** styles courtesy of https://loading.io/css/ and inspired by https://codepen.io/MattIn4D/pen/LiKFC */
+
+import { html } from '@polymer/lit-element'
+
+export const LoginViewStyles = html`
+  <style>
+    h2 {
+      margin-bottom: 0;
+    }
+    p {
+      margin-bottom: 0;
+      text-align: center;
+    }
+    input, label, button {
+      transition: all 0.25s cubic-bezier(0.53, 0.01, 0.35, 1.5);
+    }
+    form {
+      box-sizing: border-box;
+      margin: 0 auto;
+      padding: 0px 25px 30px 25px;
+      position: relative;
+      width: 270px;
+    }
+    form div {
+      position: relative;
+      width: 100%;
+    }
+    input {
+      border: 1px solid #E91E63;
+      border-radius: 60px;
+      box-sizing: border-box;
+      color: #E91E63;
+      margin-top: 30px;
+      padding: 10px 15px;
+      width: 100%;
+    }
+    input:focus {
+      background: #E91E63;
+      color: white;
+      outline: none;
+    }
+    input:valid {
+      margin-top: 30px;
+    }
+    input:focus ~ label {
+      transform: translate(0, -30px);
+    }
+    .hasText {
+      transform: translate(0, -30px);
+    }
+    input:valid ~ label {
+      font-style: italic;
+      text-transform: uppercase;
+      transform: translate(-40px, -30px) scale(0.6);
+    }
+    label {
+      box-sizing: border-box;
+      color: #E91E63;
+      position: absolute;
+      padding: 10px 15px;
+      margin-top: -40px;
+      pointer-events: none;
+      width: 100%;
+    }
+    button {
+      background-color: white;
+      border: 1px solid #E91E63;
+      border-radius: 60px;
+      color: #E91E63;
+      display: block;
+      line-height: 0;
+      margin: 35px auto 0;
+      padding: 20px 15px;
+    }
+    button:hover {
+      background-color: #E91E63;
+      color: white;
+      cursor: pointer;
+    }
+  </style>
+`
--- src/components/views/login-view.js
+++ src/components/views/login-view.js
@@ -21,87 +21,19 @@ import { navigate } from '../../actions/app'
 
 // These are the shared styles needed by this element.
 import { SharedStyles } from '../shared-styles.js'
+import { LoginViewStyles } from './login-view-styles'
 
 // These are the elements needed by this element.
 import '@polymer/iron-form/iron-form'
 import '../base/loading-element'
 
+// style inspiration: https://dribbble.com/shots/5198105 & https://codepen.io/abergin/pen/jIplf/?editors=1100
+
 class LoginView extends connect(store)(PageViewElement) {
   render() {
     return html`
       ${SharedStyles}
-      <style>
-        h2 {
-          margin-bottom: 0;
-        }
-        p {
-          margin-bottom: 0;
-          text-align: center;
-        }
-        input, label, button {
-          transition: all 0.25s cubic-bezier(0.53, 0.01, 0.35, 1.5);
-        }
-        form {
-          box-sizing: border-box;
-          margin: 0 auto;
-          padding: 0px 25px 30px 25px;
-          position: relative;
-          width: 270px;
-        }
-        form div {
-          position: relative;
-          width: 100%;
-        }
-        input {
-          border: 1px solid #E91E63;
-          border-radius: 60px;
-          box-sizing: border-box;
-          color: #E91E63;
-          margin-top: 30px;
-          padding: 10px 15px;
-          width: 100%;
-        }
-        input:focus {
-          background: #E91E63;
-          color: white;
-          outline: none;
-        }
-        input:valid {
-          margin-top: 30px;
-        }
-        input:focus ~ label {
-          transform: translate(0, -30px);
-        }
-        input:valid ~ label {
-          font-style: italic;
-          text-transform: uppercase;
-          transform: translate(-40px, -30px) scale(0.6);
-        }
-        label {
-          box-sizing: border-box;
-          color: #E91E63;
-          position: absolute;
-          padding: 10px 15px;
-          margin-top: -40px;
-          pointer-events: none;
-          width: 100%;
-        }
-        button {
-          background-color: white;
-          border: 1px solid #E91E63;
-          border-radius: 60px;
-          color: #E91E63;
-          display: block;
-          line-height: 0;
-          margin: 35px auto 0;
-          padding: 20px 15px;
-        }
-        button:hover {
-          background-color: #E91E63;
-          color: white;
-          cursor: pointer;
-        }
-      </style>
+      ${LoginViewStyles}
       <section>
         ${this._loading ? html`<loading-element></loading-element>` : ''}
         ${
@@ -112,14 +44,28 @@ class LoginView extends connect(store)(PageViewElement) {
               <iron-form>
                 <form>
                   <div>
-                    <input id="email" name="email" type="email" required>
-                    <label for="email">
+                    <input id="email" name="email" type="email" required @input=${e =>
+                      (this._email = e.target.value)}>
+                    <label for="email" class="${
+                      !this._email
+                        ? ''
+                        : this._email.length > 0
+                          ? 'hasText'
+                          : ''
+                    }">
                       E-Mail
                     </label>
                   </div>
                   <div>
-                    <input id="password" name="email" type="password" minlength="8" required>
-                    <label for ="email">
+                    <input id="password" name="email" type="password" minlength="8" required @input=${e =>
+                      (this._password = e.target.value)}>
+                    <label for ="email" class="${
+                      !this._password
+                        ? ''
+                        : this._password.length > 0
+                          ? 'hasText'
+                          : ''
+                    }">
                       Password
                     </label>
                   </div>
@@ -154,19 +100,19 @@ class LoginView extends connect(store)(PageViewElement) {
         statePath({ auth }) {
           return auth.loginFailure
         }
-      }
+      },
+      _email: String,
+      _password: String
     }
   }
 
-  static get observers() {
-    return ['_isNotEmpty(_email)']
-  }
-
-  _login(e) {
+  _login() {
     const email = this.shadowRoot.querySelector('#email')
     const password = this.shadowRoot.querySelector('#password')
     if (email.validity.valid && password.validity.valid) {
       store.dispatch(startLogin(email.value, password.value))
+      this._email = ''
+      this._password = ''
     }
   }
 
@@ -176,17 +122,9 @@ class LoginView extends connect(store)(PageViewElement) {
   }
 
   stateChanged({ auth }) {
-    if (this._loading !== auth.loading) {
-      this._loading = auth.loading
-    }
-
-    if (this._user !== auth.uid) {
-      this._user = auth.uid
-    }
-
-    if (this._loginFailure !== auth.loginFailure) {
-      this._loginFailure = auth.loginFailure
-    }
+    this._loading = auth.loading
+    this._user = auth.uid
+    this._loginFailure = auth.loginFailure
   }
 }
 
