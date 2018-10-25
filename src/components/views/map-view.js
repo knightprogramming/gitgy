f90e6107016c9edd33024686506f926e2371124c
Taylor <15224340+ttstauss@users.noreply.github.com>
2018-10-25 11:52:38 -0600

    convert MapView to protected route

--- src/components/views/map-view.js
+++ src/components/views/map-view.js
@@ -10,19 +10,52 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 
 import { html } from '@polymer/lit-element'
 import { PageViewElement } from './page-view-element.js'
+import { connect } from 'pwa-helpers/connect-mixin'
+
+// This element is connected to the Redux store.
+import { store } from '../../store'
+
+// These are the actions needed by this element.
+import { navigate } from '../../actions/app'
 
 // These are the shared styles needed by this element.
 import { SharedStyles } from '../shared-styles.js'
 
-class MapView extends PageViewElement {
+class MapView extends connect(store)(PageViewElement) {
   render() {
     return html`
       ${SharedStyles}
       <section>
-        <h2>Google Map View</h2>
-        <p>Google map goes here</p>
+        ${
+          this._user
+            ? html`
+              <h2>Google Map View</h2>
+              <p>Google map goes here</p>
+            `
+            : this._isLoggedIn()
+        }
       </section>
     `
+  }
+
+  static get properties() {
+    return {
+      _user: {
+        type: String,
+        statePath({ auth }) {
+          return auth.uid
+        }
+      }
+    }
+  }
+
+  _isLoggedIn() {
+    window.history.pushState({}, '', '/')
+    store.dispatch(navigate('/'))
+  }
+
+  stateChanged({ auth }) {
+    this._user = auth.uid
   }
 }
 
